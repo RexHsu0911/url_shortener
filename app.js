4532e6b2e8947a2d8a5da65b7534f79470b2423a
Rex Hsu <aexz5476@gmail.com>
2023-08-29 20:37:59 +0800

    build: add static files

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
+// Express app
 const express = require('express')
 const app = express()
 const port = 3000
 
+// Serving static files(public) in Express
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
-  res.send('express app for url_shortener')
+  res.redirect('/shortURL')
+})
+
+app.get('/shortURL', (req, res) =>{
+  res.send('url_shortener')
 })
 
 app.listen(port, () => {
